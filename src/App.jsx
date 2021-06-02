2280052e29036508a1090dbaf1141a487d9848fb
Takanobu <61861946+takanobu68@users.noreply.github.com>
2021-06-02 16:38:41 +0900

    削除機能の追加 onClickDelete関数の作成

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,12 @@ export const App = () => {
     setTodoText("");
   };
 
+  const onclickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
+
   return (
     <>
       <div className="input-area">
@@ -34,12 +40,12 @@ export const App = () => {
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          {incompleteTodos.map((todo) => {
+          {incompleteTodos.map((todo, index) => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
                 <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onclickDelete(index)}>削除</button>
               </div>
             );
           })}
