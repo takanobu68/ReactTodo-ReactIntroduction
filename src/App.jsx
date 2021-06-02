4dc0f89b15a4e14d92946191854b0e80fb090972
Takanobu <61861946+takanobu68@users.noreply.github.com>
2021-06-02 18:16:04 +0900

    戻す機能の実装 onClickReturn関数の作成

--- src/App.jsx
+++ src/App.jsx
@@ -35,6 +35,16 @@ export const App = () => {
     setComplteTodos(newCompleteTodos);
   };
 
+  const onClickReturn = (index) => {
+    const newCompleteTodos = [...completeTodos];
+    newCompleteTodos.splice(index, 1);
+
+    const newIncompleteTodos = [...incompleteTodos, completeTodos[index]];
+
+    setComplteTodos(newCompleteTodos);
+    setIncompleteTodos(newIncompleteTodos);
+  };
+
   return (
     <>
       <div className="input-area">
@@ -63,11 +73,11 @@ export const App = () => {
       <div className="complete-area">
         <p className="title">完了のTODO</p>
         <ul>
-          {completeTodos.map((todo) => {
+          {completeTodos.map((todo, index) => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
-                <button>戻す</button>
+                <button onClick={() => onClickReturn(index)}>戻す</button>
               </div>
             );
           })}
